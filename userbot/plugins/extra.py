09149865f002aa7abcf9cf6d41b21a705f87e390
tesbot07 <70955557+tesbot07@users.noreply.github.com>
2020-09-14 11:23:47 +0700

    Update extra.py

--- userbot/plugins/extra.py
+++ userbot/plugins/extra.py
@@ -20,42 +20,6 @@ weebyfont = ['卂', '乃', '匚', '刀', '乇', '下', '厶', '卄', '工', '丁
 import emoji
 from googletrans import Translator
 from userbot.utils import admin_cmd
-
-
-@borg.on(admin_cmd("tr ?(.*)"))
-async def _(event):
-    if event.fwd_from:
-        return
-    if "trim" in event.raw_text:
-        return
-    input_str = event.pattern_match.group(1)
-    if event.reply_to_msg_id:
-        previous_message = await event.get_reply_message()
-        text = previous_message.message
-        lan = input_str or "id"
-    elif "|" in input_str:
-        lan, text = input_str.split("|")
-    else:
-        await event.edit("`.tr LanguageCode` as reply to a message")
-        return
-    text = emoji.demojize(text.strip())
-    lan = lan.strip()
-    translator = Translator()
-    try:
-        translated = translator.translate(text, dest=lan)
-        after_tr_text = translated.text
-        output_str = """
-         Source **( {} )**
-Translate ke **( {} )**
-
-Hasil : {}""".format(
-            translated.src,
-            lan,
-            after_tr_text
-        )
-        await event.edit(output_str)
-    except Exception as exc:
-        await event.edit(str(exc))
         
     
 @borg.on(admin_cmd("ud (.*)"))
